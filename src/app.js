aada78e5eb07c370ded6cd7cdd3ab1e3b6c9a86e
HectorMtz22 <hec.gre81@gmail.com>
2019-10-20 09:40:13 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.set('port', process.env.PORT || 4000);
 
 //middlewares
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
