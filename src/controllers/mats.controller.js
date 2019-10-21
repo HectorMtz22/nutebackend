3892f7f1b6f668780c6d5e973d2f1f51059418e1
HectorMtz22 <hec.gre81@gmail.com>
2019-10-21 17:26:17 -0500

    Solucionado blanco

--- src/controllers/mats.controller.js
+++ src/controllers/mats.controller.js
@@ -8,11 +8,16 @@ matsCtrl.getMats = async (req, res) => {
 }
 
 matsCtrl.createMats = async (req, res) => {
-    const { matname } = req.body;
-    const newMat = new Mat({matname});
-    newMat.user = req.userId;
-    await newMat.save();
-    res.json('Mat created');
+    const matCreated = await Mat.findOne({matname: req.body.matname})
+    if (matCreated) {
+        return res.json({message: 'Materia ya creada anteriormente'})
+    } else {
+        const { matname } = req.body;
+        const newMat = new Mat({matname});
+        newMat.user = req.userId;
+        await newMat.save();
+        res.json('Mat created');
+    }
 }
 
 matsCtrl.deleteMats = async (req, res) => {
